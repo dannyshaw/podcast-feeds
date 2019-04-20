2c4bec24cee17d3e4f12e37a3c6f707f663b7606
Danny Shaw <code@dannyshaw.io>
2019-04-20 12:41:59 +1000

    init

---
+++ podcast-feeds/generate-seinfeld.py
@@ -0,0 +1,38 @@
+from feedgen.feed import FeedGenerator
+from os import listdir, rename
+from os.path import isfile, join
+from s3upload import upload_file
+
+fg = FeedGenerator()
+fg.id('http://dannyshaw.github.io/podcast-feeds')
+fg.title('Danny\'s Podcasts')
+fg.author({'name': 'Danny Shaw', 'email': 'code@dannyshaw.io'})
+fg.link(href='http://dannyshaw.github.io/podcast-feeds', rel='alternate')
+fg.subtitle('My personal rss feed...')
+fg.link(href='http://dannyshaw.github.io/podcast-feeds/rss.xml', rel='self')
+fg.language('en')
+
+FILES = '/home/danny/Downloads/audio'
+
+episodes = sorted([f for f in listdir(FILES) if isfile(join(FILES, f))])
+
+for ep in episodes:
+    upload_file(join(FILES, ep), 'danny.podcasts.seinfeld', ep)
+    # fe = fg.add_entry()
+    # fe.id(f'http://dannyshaw.github.io/podcast-feeds/seineld/{ep}')
+    # fe.title(ep)
+    # fe.link(href=f'http://dannyshaw.github.io/podcast-feeds/seineld/{ep}')
+
+# ep.rename()
+# Write the RSS feed to a file
+# fg.rss_file('rss.xml')
+
+
+def rename_files():
+    FILES = '/home/danny/Downloads/audio'
+    episodes = [f for f in listdir(FILES) if isfile(join(FILES, f))]
+    for ep in episodes:
+        file_name = join(FILES, ep)
+        new_name = (ep.replace(' - ', '-').replace(' ', '-').replace('(', '')
+                    .replace(')', '').replace(',-', '-').lower())
+        rename(file_name, join(FILES, new_name))
---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup, find_packages
+
+try:
+    long_description = open("README.rst").read()
+except IOError:
+    long_description = ""
+
+setup(
+    name="podcast-feeds",
+    version="0.1.0",
+    description="My personal podcast feeds",
+    license="MIT",
+    author="Danny Shaw",
+    packages=find_packages(),
+    install_requires=[],
+    long_description=long_description,
+    classifiers=[
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2.7",
+    ]
+)
